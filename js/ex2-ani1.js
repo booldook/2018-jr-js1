e5e1fd845451e1e7be1fdbc412f40baa911c8b0c
booldook <booldook@gmail.com>
2018-12-24 14:02:01 +0900

    이미지 추가

--- js/ex2-ani1.js
+++ js/ex2-ani1.js
@@ -2,11 +2,5 @@ console.log(	$("#bt_run")[0]	);
 console.log(	$(document.getElementById("bt_run"))	);
 
 $("#bt_run").click(function(){
-	$("#box1").animate({
-		"margin-left":"90%"
-	}, 1000, "swing", function(){
-		$(this).css({
-			"margin-left":0
-		});
-	});
+	$(".box").eq(0).animate({""})
 });
