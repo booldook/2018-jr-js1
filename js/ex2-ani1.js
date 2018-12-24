3166f96c5411a2c76a56272875697d75ca0fca15
booldook <booldook@gmail.com>
2018-12-24 17:29:12 +0900

    애니메이션 심화

--- js/ex2-ani1.js
+++ js/ex2-ani1.js
@@ -1,6 +1,30 @@
 console.log(	$("#bt_run")[0]	);
 console.log(	$(document.getElementById("bt_run"))	);
 
-$("#bt_run").click(function(){
-	$(".box").eq(0).animate({""})
+/* $("#bt_run").click(function(){
+	$(".box").eq(0).stop().animate({"left":"90%"}, 1000, function(){
+		$(this).stop().animate({"left":0}, 1000);
+		$(".box").eq(1).stop().animate({"left":"90%"}, 1000, function(){
+			$(this).stop().animate({"left":0}, 1000);
+			$(".box").eq(2).stop().animate({"left":"90%"}, 1000, function(){
+				$(this).stop().animate({"left":0}, 1000);
+			});
+		});
+	});
+}); */
+function boxAni(){
+	var speed = 1000;
+	$(".box").each(function(i){
+		$(this).delay(i*speed).animate({"left":"90%"}, speed, function(){
+			$(this).animate({"left":0}, speed);
+			//console.log($(this).index(), $(".box").length);
+			if($(this).index() == $(".box").length - 1) {
+				$("#bt_run").prop("disabled", false);
+			}
+		});
+	});
+}
+$("#bt_run").on("click", function(){
+	$(this).prop("disabled", true);
+	boxAni();
 });
