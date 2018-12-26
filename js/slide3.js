03d3b6298af668bc6653b8e96ac93fe0cb9c53b5
booldook <booldook@gmail.com>
2018-12-26 15:05:40 +0900

    booldook

--- js/slide3.js
+++ js/slide3.js
@@ -12,32 +12,31 @@ $.ajax({
 			</li>`;
 			$(".banners > ul").append(html);
 		}
-		var n = 1	;
-		$(window).on("resize", function(){
-			$(".banners").height($(".banner").eq(0).height());
-		}).trigger("resize");
+		$("body").imagesLoaded(function() {
+			$("#loader").hide();
+			var n = 1	;
+			$(window).on("resize", function(){
+				$(".banners").height($(".banner").eq(0).height());
+			}).trigger("resize");
 
-		$(".banners > ul").append($(".banner").eq(0).clone());
-		var end = $(".banner").length - 1;
-		var interval = setInterval(ani, 2000);
+			$(".banners > ul").append($(".banner").eq(0).clone());
+			var end = $(".banner").length - 1;
+			var interval = setInterval(ani, 2000);
 
-		$('#container').imagesLoaded( function() {
-			// images have loaded
-		});
-
-		$(".banner").each(function(i){
-			$(this).css({"left":(i*100)+"%"});
-		});
-
-		function ani(){
-			$(".banners > ul").stop().animate({"left":(-n*100)+"%"}, 500, function(){
-				if(n == end) {
-					$(this).css({"left":0});
-					n = 1;
-				}
-				else n++;
+			$(".banner").each(function(i){
+				$(this).css({"left":(i*100)+"%"});
 			});
-		}
+
+			function ani(){
+				$(".banners > ul").stop().animate({"left":(-n*100)+"%"}, 500, function(){
+					if(n == end) {
+						$(this).css({"left":0});
+						n = 1;
+					}
+					else n++;
+				});
+			}
+		});
 	},
 	error: function(xhr){
 		console.log(xhr);
