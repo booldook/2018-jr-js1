09834582c5dea9ae0b1bd8061f7a465a6b941f99
booldook <booldook@gmail.com>
2018-12-26 14:30:21 +0900

    booldook

--- js/slide3.js
+++ js/slide3.js
@@ -1,26 +1,46 @@
 $.ajax({
 	url: "../json/slide3.json",
-	type: "post",
+	type: "get",
 	dataType: "json",
 	data : {},
 	success: function(data){
+		/*
+		for(var i=0; i<data.result.length; i++) {
+			console.log(data.result[i]);
+		}
+		*/
+		var html = '';
+		for(var i in data.result) {
+			/* html  = '<li class="banner">';
+			html += '<img src="'+data.result[i].src+'" class="img">';
+			html += '</li>'; */
 
+			html = `
+			<li class="banner">
+				<img src="${data.result[i].src}" class="img">
+			</li>`;
+
+			$(".banners>ul").append(html);
+		}
+		
 	},
-	error: function(x){
-
+	error: function(xhr){
+		console.log(xhr);
 	}
 });
 
 
 var n = 1	;
 
+$(window).on("resize", function(){
+	$(".banners").height($(".banner").eq(0).height());
+}).trigger("resize");
+
 $(".banners > ul").append($(".banner").eq(0).clone());
 var end = $(".banner").length - 1;
 var interval = setInterval(ani, 2000);
 
-$(window).on("resize", function(){
-	$(".banners").height($(".banner").eq(0).height());
-}).trigger("resize");
+
 
 $(".banner").each(function(i){
 	$(this).css({"left":(i*100)+"%"});
