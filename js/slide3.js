4c18452c8e1942ed79be23d5c005d8e16d0930fc
booldook <booldook@gmail.com>
2018-12-26 14:55:33 +0900

    booldook

--- js/slide3.js
+++ js/slide3.js
@@ -10,37 +10,37 @@ $.ajax({
 			<li class="banner">
 				<img src="${data.result[i].src}" class="img">
 			</li>`;
-			$(".banners>ul").append(html);
+			$(".banners > ul").append(html);
 		}
-		
+		var n = 1	;
+		$(window).on("resize", function(){
+			$(".banners").height($(".banner").eq(0).height());
+		}).trigger("resize");
+
+		$(".banners > ul").append($(".banner").eq(0).clone());
+		var end = $(".banner").length - 1;
+		var interval = setInterval(ani, 2000);
+
+		$('#container').imagesLoaded( function() {
+			// images have loaded
+		});
+
+		$(".banner").each(function(i){
+			$(this).css({"left":(i*100)+"%"});
+		});
+
+		function ani(){
+			$(".banners > ul").stop().animate({"left":(-n*100)+"%"}, 500, function(){
+				if(n == end) {
+					$(this).css({"left":0});
+					n = 1;
+				}
+				else n++;
+			});
+		}
 	},
 	error: function(xhr){
 		console.log(xhr);
 	}
 });
 
-var n = 1	;
-
-$(window).on("resize", function(){
-	$(".banners").height($(".banner").eq(0).height());
-}).trigger("resize");
-
-$(".banners > ul").append($(".banner").eq(0).clone());
-var end = $(".banner").length - 1;
-var interval = setInterval(ani, 2000);
-
-
-
-$(".banner").each(function(i){
-	$(this).css({"left":(i*100)+"%"});
-});
-
-function ani(){
-	$(".banners > ul").stop().animate({"left":(-n*100)+"%"}, 500, function(){
-		if(n == end) {
-			$(this).css({"left":0});
-			n = 1;
-		}
-		else n++;
-	});
-}
