26e6d69f8cbf46a3ee0388fefa153133d5c22f87
booldook <booldook@gmail.com>
2018-12-27 10:58:24 +0900

    booldook

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,18 +1,38 @@
-var log = console.log;
-var direction = null;
-var pos = [];
-var now = 0;
+var log = console.log;		//console.log() 메서드를 log 변수에 넣는다
+var direction = null;			//direction 저장 변수 (- 휠을 위로 올림/+ 휠을 아래로 내림)
+var pos = [];							//각각의 페이지가 body로부터 떨어진 y 값
+var now = 0;							//현재 보고있는 페이지
+/*
+mousewheel(ff를제외한 모든 브라우저에서 실행됨) 이벤트와 
+DOMMouseScroll(firefox에서 실행됨) 이벤트를 동시에 선언
+*/
 $(window).on('mousewheel DOMMouseScroll', function(e){
-	e.preventDefault();
-	direction = e.originalEvent.deltaY;
-	$(".box").each(function(i){
-		pos[i] = $(this).offset().top;
+	e.preventDefault();	//휠의 기본 기능을 막음(이벤트의 기본 기능 제거)
+	direction = e.originalEvent.deltaY;	//휠의 방향을 direction변수에 저장
+
+	//각각의 페이지의 body로 부터 떨어진 값을 pos 배열 변수에 저장
+	$(".page").each(function(i){
+		pos[i] = $(this).offset().top;	//offset(): 부모로 부터의 거리값
 	});
 	
-	if(direction > 0 ) {
-		//$("html, body").stop().animate({"scrollTop":pos[2]+"px"}, 500);
+	now = pos.length - 1;
+	//for(var i=0; i<pos.length; i++) {
+	for(var i in pos) {
+		//log("e.pageY : " + e.pageY);
+		//log("pos["+i+"] : " + pos[i]);
+		if(e.pageY < pos[i]) {
+			now = i - 1;
+			break;
+		}
+	}
+	log(now, direction);
+	//휠의 방향에 따른 애니메이션 분기
+	if(direction < 0) {
+		if(now > 0) now--;
+		$("html, body").stop().animate({"scrollTop":pos[now]+"px"}, 500);
 	}
 	else {
-		//$("html, body").stop().animate({"scrollTop":pos[1]+"px"}, 500);
+		if(now < pos.length - 1) now++;
+		$("html, body").stop().animate({"scrollTop":pos[now]+"px"}, 500);
 	}
 });
