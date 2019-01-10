9e557bbdf8214f10f6b515c2b9945ead5914a1fc
booldook <booldook@gmail.com>
2019-01-10 14:33:26 +0900

    2019.01.10

--- js/2019.01.js
+++ js/2019.01.js
@@ -9,7 +9,23 @@ $("#bt2").click(function(){
 	$(".box").css({"width":"200px", "height":"200px", "float":"left"});
 });
 
-
+/*
+var i = 1;
+$("#bt3").click(function(){
+	$(".box2").append('<div>'+(i++)+'</div>');
+	//$(".box2").append(`<div>${i++}</div>`);
+});
+*/
+$("#bt3").click(function(){
+	$(".box2").empty();
+	var num = new Array();
+	for(var i=0; i<45; i++){
+		num[i] = i + 1 + "번";
+	}
+	for(var i in num) {
+		$(".box2").append(`<div>${num[i]}</div>`);
+	}
+});
 
 
 
