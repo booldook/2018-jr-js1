292e8ecc738953ceb5ce3524d93d6df6fd4ad7f9
booldook <booldook@gmail.com>
2019-01-15 15:34:49 +0900

    IF 문 예제1

--- js/2019.02.js
+++ js/2019.02.js
@@ -1,5 +1,13 @@
 const log = console.log;
 
+var pos = $("#box1").css("margin-top");
+var tar = Number(pos.replace("px", "")) + 200;
+log(pos, tar);
 $("#bt1").click(function(){
-	$("#box1").stop().fadeToggle(1000);
+	if($("#box1").css("margin-top") == pos) {
+		$("#box1").stop().animate({"margin-top":tar+"px"});
+	}
+	else {
+		$("#box1").stop().animate({"margin-top":pos});
+	}
 });
